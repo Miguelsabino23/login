9f9b2ba638330c21b0db45514edce32c0b8fa397
miguelsabino23 <miguel2012palhada@gmail.com>
2024-07-24 20:09:13 -0300

    feat: add footer component

---
+++ src/ui/components/Footer.js
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer
+      style={{
+        width: "100%",
+        backgroundColor: "#0078f0",
+        height: "6rem",
+        position: "absolute",
+        bottom: "0",
+      }}
+    ></footer>
+  );
+}
