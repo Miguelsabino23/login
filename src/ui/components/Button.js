427cdfdf614774dd0be7bcff1392c30283dd66b1
miguelsabino23 <miguel2012palhada@gmail.com>
2024-07-24 20:11:36 -0300

    feat: add button component

---
+++ src/ui/components/Button.js
@@ -0,0 +1,17 @@
+export default function Button() {
+  return (
+    <button
+      style={{
+        backgroundColor: "#0078F0",
+        color: "#ffffff",
+        border: "none",
+        width: "100%",
+        padding: ".8rem 0",
+        fontSize: "1rem",
+        borderRadius: ".2rem",
+      }}
+    >
+      ENTRAR
+    </button>
+  );
+}
